de6ed7eca3f04f8db18f4998bb7ee149ca218f81
loopystack19 <sanjaygreg19@gmail.com>
2025-10-13 17:29:59 +0300

    we just worked on a simple grade checker

--- If-Else statements/if_else.py
+++ If-Else statements/if_else.py
@@ -1,14 +1,32 @@
-#Simple grade checker using  if, elif and else
+#An intermediate question on control structures
 
-score=int(input("What was your score in the last maths test?: "))
+import math
 
-if score > 90 and score <=100:
-    print("You scored an A ")
-elif score >=80 and score <=90:
-    print("You scored a B")
-elif score >=70 and score < 80:
-    print("You scored a C")
-elif score>=60 and score < 70:
-    print("You scored a D")
+math_score=int(input("What did you score in the maths test bro?: "))
+
+science_score=int(input("What did you score in the science test bro?: "))
+
+english_score=int(input("What did you score in your english test?: "))
+
+average_score=math.floor((math_score + science_score + english_score)/3)
+
+if(average_score >=90 and (math_score>=80 and science_score>=80 and english_score>=80) ):
+    grade="A"
+    print(grade)
+elif(average_score>=80 and ((math_score >=75 and science_score>=75 and english_score<=75) or (math_score >=75 and science_score<=75 and english_score>=75) or (math_score <=75 and science_score>=75 and english_score>=75))):
+    grade="A"
+    print(grade)
+elif(average_score>=70 and (((math_score>=60 and science_score>=60 and english_score>=60)))):
+    grade="B"
+    print(grade)
+elif(average_score>=60 and ((math_score<=50 or science_score<=50 or english_score<=50))):
+    grade="C"
+    print(grade)
+elif(math_score<=40 or science_score<=40 or english_score<=40):
+    grade="FAIL"
+    print(grade)
 else:
-    print("You failed")
+    grade="D"
+    print(grade)
+
+
