c1cdc0620cbe5c125e4675189c8a48d61c04000b
loopystack19 <sanjaygreg19@gmail.com>
2025-10-20 20:51:11 +0300

    using the count function in a real world programme

---
+++ tuples/test.js
@@ -0,0 +1,8 @@
+
+
+let arrayLength=[1,2,3,4,5,5,5,6,7,8,8,8,9,10]
+
+for(let i=0; i<arrayLength; i++){
+
+    console.log(arrayLength[i])
+}
--- tuples/test.py
+++ tuples/test.py
@@ -1,11 +1,14 @@
-#print the squares of all the numbers in a list
+#Count the occurence of a particular element in a list
 
 import math
 
-my_list=[1,2,3,4,5,6,7,8,9,10]
+my_list=[1,2,2,2,2,3,4,5,6]
 
-for number in my_list:
-    print(math.pow(number,2))
+my_count=my_list.count(2)
+
+print(f"2 appears : {my_count}")
+        
+    
 
 
     
