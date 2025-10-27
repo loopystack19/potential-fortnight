87426fd1a17bb5ccb3a66df33d5063a4e9066d05
loopystack19 <sanjaygreg19@gmail.com>
2025-10-27 15:14:46 +0300

    2d list are like 2 dimension arrays in other languages , beasically its made of individual list

--- 2d_list/2d_list.py
+++ 2d_list/2d_list.py
@@ -13,3 +13,18 @@ for row in range(0, len(groceries)):
     print()
     
 #2d list are like 2 dimension arrays in other languages , beasically its made of individual list
+
+#sample programme creating a 2d keypad that we see on phones
+
+list1=[1,2,3]
+list2=[4,5,6]
+list3=[7,8,9]
+list4=["*",0,"#"]
+
+phone=[list1,list2,list3,list4]
+
+
+for row in phone:
+    for element in row:
+        print(element ,end="")
+    print()
