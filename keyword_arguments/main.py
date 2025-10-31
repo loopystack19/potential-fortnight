e9e406c1546f4058cb97876cf32eb2fbecdf43b4
loopystack19 <sanjaygreg19@gmail.com>
2025-10-31 18:23:50 +0300

    keyword arguments are just arguments preceeded by identifiers

---
+++ keyword_arguments/main.py
@@ -0,0 +1,13 @@
+#keyword arguments=an argument preceeded by an identifier, it helps with readability order of arguements doesn't matter
+
+#sample programme-1
+
+def hello(greeting, title, first_name,last_name):
+    print(f"{greeting}, {title} {first_name} {last_name}")
+    
+hello("Good Morning", title="MR.", first_name="Jafar",last_name="Hussein")
+
+#when we have keyword arguements the order doesn't really matter, but you should ensure they come after positional arguements
+
+
+
